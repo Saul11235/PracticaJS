3417da55d96b80c130de6f1f0b3b1c2dc6dad980
Edwin Saul <edwinsaulpm@gmail.com>
2023-03-03 23:08:54 -0500

    primeros ejemplos de codigo

---
+++ ejemplos_JS/e002_comentario.js
@@ -0,0 +1,6 @@
+// sou una linea de codigo si me ejcuto
+console.log("yo si me ejecuto") // soy un comentario
+/*
+ hola yo soy un comentario de varias
+  varias lineas y tampoco me ejecuto
+ * */
---
+++ ejemplos_JS/e003_tipos_de_datos.js
@@ -0,0 +1,5 @@
+/*
+ * TIPOS DE DATOS EN JS
+ *
+ *
+ * /
